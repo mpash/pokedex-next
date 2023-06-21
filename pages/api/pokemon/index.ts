7600530490a027502eb8b15bab590bf3f2d79ba3
Mitch Pash <mpash@bushelpowered.com>
2023-06-20 21:03:11 -0600

    wip

--- pages/api/pokemon/index.ts
+++ pages/api/pokemon/index.ts
@@ -1,35 +1,8 @@
-import { Prisma } from '@prisma/client'
-import { prisma } from '@src/utils/prisma'
-import { isEmpty, isNumber } from 'lodash/fp'
-import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '@src/utils/prisma-edge'
+import { Prisma, Pokemon, Region } from '@prisma/client'
+import { NextRequest } from 'next/server'
 
-export type Pokemon = {
-  id: number
-  name: string
-  number: string
-  japaneseName: string
-  descriptionX: string
-  descriptionY: string
-  hp: number
-  height: number
-  weight: number
-  image: string
-  subVariant: number
-  types: PokemonType[]
-  weaknesses: PokemonType[]
-  primaryColor: Color
-  region: {
-    id: number
-    name: string
-  }
-  evolvesFrom?: Pokemon[]
-  evolvesTo?: Pokemon[]
-  evolutionChain?: string[]
-  evolutions?: Omit<
-    Pokemon[],
-    'types' | 'weaknesses' | 'primaryColor' | 'region'
-  >
-}
+export const config = { runtime: 'edge' }
 
 type Color = {
   r: number
@@ -37,11 +10,29 @@ type Color = {
   b: number
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<any>,
-) {
-  const id = req.query.id ? parseInt(req.query.id as string) : undefined
+type PokemonListItem = {
+  id: number
+  name: string
+  number: string
+  japaneseName: string
+  descriptionX: string
+  descriptionY: string
+  hp: number
+  image: string
+  height: number
+  weight: number
+  subVariant: number
+  primaryColor: Color
+  types: string[]
+  weaknesses: string[]
+  region: Region
+}
+
+export default async function handler(req: NextRequest) {
+  const { searchParams } = new URL(req.url)
+  const id = searchParams.has('id')
+    ? parseInt(searchParams.get('id') as string)
+    : undefined
 
   if (id) {
     const pokemon = await prisma.pokemon.findUnique({
@@ -60,77 +51,43 @@ export default async function handler(
     if (!pokemon) {
       throw new Error('Unable to find a Pokemon with the given ID: ' + id)
     }
-    return res.status(200).json({
-      data: {
-        ...pokemon,
-        types: pokemon.types.map(t => t.type),
-        weaknesses: pokemon.weaknesses.map(t => t.type),
-      },
-    })
+    return new Response(
+      JSON.stringify({
+        data: {
+          ...pokemon,
+          types: pokemon.types.map(t => t.type),
+          weaknesses: pokemon.weaknesses.map(t => t.type),
+        },
+      }),
+    )
   }
 
-  const pageSize = req.query.pageSize
-    ? parseInt(req.query.pageSize as string)
+  const pageSize = searchParams.has('pageSize')
+    ? parseInt(searchParams.get('pageSize') as string)
     : 20
 
-  const lastId = req.query.lastId
-    ? parseInt(req.query.lastId as string)
+  const lastId = searchParams.has('lastId')
+    ? parseInt(searchParams.get('lastId') as string)
     : undefined
 
-  const lastIdCursor = lastId
-    ? {
-        cursor: { id: lastId },
-        skip: 1,
-      }
-    : undefined
+  const lastIdCursor = lastId ? { cursor: { id: lastId }, skip: 1 } : undefined
 
-  let query = req.query.q ? (req.query.q as string) : undefined
+  let query = searchParams.get('q') ?? undefined
   const isWeaknessCheck = query?.startsWith('weak:')
   query = query?.replace('weak:', '')
-  const hideVariantsAndQuery: Prisma.PokemonWhereInput['AND'] = req.query
-    .hideVariants
-    ? {
-        subVariant: 0,
-      }
-    : {}
 
   const whereQuery: Prisma.PokemonWhereInput = {}
 
-  if (!isEmpty(hideVariantsAndQuery)) {
-    whereQuery.AND = hideVariantsAndQuery
+  if (searchParams.has('hideVariants')) {
+    whereQuery.AND = { subVariant: 0 }
   }
 
   if (query) {
     whereQuery.OR = [
-      {
-        name: {
-          contains: query,
-          mode: 'insensitive',
-        },
-      },
-      {
-        japaneseMeta: {
-          name: {
-            contains: query,
-            mode: 'insensitive',
-          },
-        },
-      },
-      {
-        types: {
-          some: {
-            type: {
-              contains: query,
-              mode: 'insensitive',
-            },
-          },
-        },
-      },
-      {
-        number: {
-          contains: query,
-        },
-      },
+      { name: { contains: query, mode: 'insensitive' } },
+      { japaneseMeta: { name: { contains: query, mode: 'insensitive' } } },
+      { types: { some: { type: { contains: query, mode: 'insensitive' } } } },
+      { number: { contains: query } },
     ]
   }
 
@@ -159,28 +116,14 @@ export default async function handler(
 
     if (queries.length) {
       const typesQuery: Prisma.PokemonWhereInput['OR'] = {
-        types: {
-          some: {
-            type: {
-              in: queries,
-            },
-          },
-        },
+        types: { some: { type: { in: queries } } },
       }
       const weaknessQuery: Prisma.PokemonWhereInput['OR'] = {
-        weaknesses: {
-          some: {
-            type: {
-              in: queries,
-            },
-          },
-        },
+        weaknesses: { some: { type: { in: queries } } },
       }
 
       const nameSearchQuery: Prisma.PokemonWhereInput['OR'] = {
-        name: {
-          search: queries.join(' | '),
-        },
+        name: { search: queries.join(' | ') },
       }
 
       whereQuery.OR = [
@@ -210,13 +153,7 @@ export default async function handler(
 
     if (isWeaknessCheck) {
       const weaknessQuery: Prisma.PokemonWhereInput['OR'] = {
-        weaknesses: {
-          some: {
-            type: {
-              search: query,
-            },
-          },
-        },
+        weaknesses: { some: { type: { search: query } } },
       }
       whereQuery.OR = [...(whereQuery.OR as any[]), weaknessQuery]
     }
@@ -238,73 +175,97 @@ export default async function handler(
 
   const lastPokemonId = pokemon[pokemon.length - 1]?.id
 
-  const generateBaseUrl = () => {
-    const baseUrl = new URL(
-      '/api/pokemon',
-      process.env.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000',
-    )
-    baseUrl.searchParams.set('pageSize', pageSize.toString())
-    query && baseUrl.searchParams.append('q', query)
-    return baseUrl
-  }
+  return new Response(
+    JSON.stringify({
+      data: pokemon.map(
+        ({
+          id,
+          name,
+          number,
+          japaneseMeta,
+          descriptionX,
+          descriptionY,
+          hp,
+          image,
+          height,
+          weight,
+          subVariant,
+          primaryColor,
+          types,
+          weaknesses,
+          region,
+        }) =>
+          ({
+            id,
+            name,
+            number,
+            japaneseName: japaneseMeta?.name,
+            descriptionX,
+            descriptionY,
+            hp,
+            image,
+            height,
+            weight,
+            subVariant,
+            primaryColor: {
+              r: primaryColor?.r,
+              g: primaryColor?.g,
+              b: primaryColor?.b,
+            },
+            types: types.map(type => type.type),
+            weaknesses: weaknesses.map(type => type.type),
+            region,
+          } as PokemonListItem),
+      ),
+      pagination: {
+        pageSize,
+        nextPage: generateNextPageUrl(pokemon, pageSize, {
+          query,
+          lastPokemonId,
+        }),
+      },
+    }),
+  )
+}
 
-  const generateNextPageUrl = () => {
+const generateBaseUrl = (pageSize: number, query?: string) => {
+  const baseUrl = new URL(
+    '/api/pokemon',
+    process.env.VERCEL_URL ?? 'http://localhost:3000',
+  )
+  baseUrl.searchParams.set('pageSize', pageSize.toString())
+  query && baseUrl.searchParams.append('q', query)
+  return baseUrl
+}
+
+function generateNextPageUrl(
+  pokemon: Pokemon[],
+  pageSize = 20,
+  params: {
+    query?: string
+    lastPokemonId?: number
+  },
+) {
+  const { query, lastPokemonId } = params
+  return () => {
     if (!pokemon.length || pokemon.length < pageSize) return null
-    const nextPage = generateBaseUrl()
+    const nextPage = generateBaseUrl(pageSize, query)
     lastPokemonId &&
       nextPage.searchParams.append('lastId', lastPokemonId.toString())
     return nextPage
   }
-
-  return res.status(200).json({
-    data: pokemon.map(
-      pokemon =>
-        ({
-          id: pokemon.id,
-          name: pokemon.name,
-          number: pokemon.number,
-          japaneseName: pokemon?.japaneseMeta?.name,
-          descriptionX: pokemon.descriptionX,
-          descriptionY: pokemon.descriptionY,
-          hp: pokemon.hp,
-          image: pokemon.image,
-          height: pokemon.height,
-          weight: pokemon.weight,
-          subVariant: pokemon.subVariant,
-          primaryColor: {
-            r: pokemon.primaryColor?.r,
-            g: pokemon.primaryColor?.g,
-            b: pokemon.primaryColor?.b,
-          },
-          types: pokemon.types.map(type => type.type),
-          weaknesses: pokemon.weaknesses.map(type => type.type),
-          region: pokemon.region,
-        } as Pokemon),
-    ),
-    pagination: {
-      pageSize,
-      nextPage: generateNextPageUrl(),
-    },
-  })
 }
 
 function appendRangeQuery(range: string, whereQuery: Prisma.PokemonWhereInput) {
   const [start, end] = range.split('-')
   const startId = parseInt(start)
 
-  const rangeQuery: any = {
+  const rangeQuery: Prisma.PokemonWhereInput['OR'] = {
     sourceId: {
       gte: startId,
+      ...(end ? { lte: parseInt(end) } : {}),
     },
   }
 
-  if (end) {
-    const endId = parseInt(end)
-    rangeQuery.sourceId.lte = endId
-  }
-
-  whereQuery.OR = [
-    ...((whereQuery.OR as Prisma.PokemonWhereInput['OR'][]) ?? []),
-    rangeQuery,
-  ]
+  whereQuery.OR = [...((whereQuery.OR as []) ?? []), rangeQuery]
 }
--- src/utils/prisma-edge.ts
+++ src/utils/prisma-edge.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 
 declare const global: Global & {
   prisma?: PrismaClient
